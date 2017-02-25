bb1ece1f5a0b5d636420b829eede0fdb78f8e6b5
Tuan Anh Le <anhlt37@fsoft.com.vn>
2017-02-25 22:25:35 +0900

    Cmment out line count

--- chapter1/linecount.c
+++ chapter1/linecount.c
@@ -9,26 +9,26 @@
 #include <stdio.h>
 
 /* count lines in input */
-main()
-{
-	int c;
-	int n1;    // count the line
-	int n2;    // count the tab
-	int n3;    // count the blank
-
-	n1 = n2 = n3 = 0;
-
-	while ((c=getchar()) != EOF) {
-		if (c ==  '\n') {
-			++n1;
-		} else if (c == '\t') {
-			+n2;
-		} else if (c == ' ') {
-			++n3;
-		}
-	}
-
-	printf("lines : %d\n", n1);
-	printf("tabs  : %d\n", n2);
-	printf("blank : %d\n", n3);
-}
+//main()
+//{
+//	int c;
+//	int n1;    // count the line
+//	int n2;    // count the tab
+//	int n3;    // count the blank
+//
+//	n1 = n2 = n3 = 0;
+//
+//	while ((c=getchar()) != EOF) {
+//		if (c ==  '\n') {
+//			++n1;
+//		} else if (c == '\t') {
+//			+n2;
+//		} else if (c == ' ') {
+//			++n3;
+//		}
+//	}
+//
+//	printf("lines : %d\n", n1);
+//	printf("tabs  : %d\n", n2);
+//	printf("blank : %d\n", n3);
+//}
