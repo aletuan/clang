cc7ad9feb07f9f4b0883c32a28440c2cb0a9ff4b
Tuan Anh Le <anhlt37@fsoft.com.vn>
2017-02-25 19:13:19 +0900

    Adding counting tab and blank. But Blank is not working.

--- chapter1/charcount.c
+++ chapter1/charcount.c
@@ -8,17 +8,17 @@
 
 #include <stdio.h>
 
-/* count characters in input; 1st version */
-main()
-{
-	long nc;
-
-	/*
-	while ( (c = getchar()) != EOF)
-		++nc;
-	*/
-	for (nc = 0; (getchar() != EOF); nc++)
-		;
-
-	printf("\nnc = %ld\n", nc);
-}
+/* count characters in input; 2nd version */
+//main()
+//{
+//	long nc;
+//
+//	/*
+//	while ( (c = getchar()) != EOF)
+//		++nc;
+//	*/
+//	for (nc = 0; (getchar() != EOF); nc++)
+//		;
+//
+//	printf("\nnc = %ld\n", nc);
+//}
