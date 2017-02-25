8b3494229bbfcf116f51b73fe064dc58e6c10b34
Tuan Anh Le <anhlt37@fsoft.com.vn>
2017-02-25 22:43:31 +0900

    Count number of digit from input

---
+++ chapter1/arrays.c
@@ -0,0 +1,35 @@
+/*
+ * arrays.c
+ *
+ *  Created on: Feb 25, 2017
+ *      Author: ace
+ */
+
+
+#include <stdio.h>
+
+main()
+{
+	int c, i, nwhite, nother;
+	int ndigit[10];
+
+	nwhite = nother = 0;
+
+	for (i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+
+	while ((c=getchar()) != EOF) {
+		if (c >= '0' && c <= '9')
+			++ndigit[c - '0'];
+		else if( c == ' ' || c == '\t' || c == '\n')
+			++nwhite;
+		else
+			++nother;
+	}
+
+	printf("digits = ");
+	for (i = 0; i < 10; ++i)
+		printf("%3d", ndigit[i]);
+
+	printf(", white space = %d, other = %d\n", nwhite, nother);
+}
--- chapter1/wordcount.c
+++ chapter1/wordcount.c
@@ -12,25 +12,25 @@
 #define OUT 0   // outside a word
 
 /* counting number of words in input */
-int main()
-{
-	int c;
-	int nw;    //number of words in input
-
-	int state = OUT;
-
-	nw = 0;
-
-	while ((c=getchar()) != EOF) {
-		if (c == ' ' || c == '\t' || c == '\n') {
-			state = OUT;
-		} else if (state == OUT) {
-			state = IN;
-			++nw;
-		}
-	}
-
-	printf("%d\n", nw);
-
-	return 0;
-}
+//int main()
+//{
+//	int c;
+//	int nw;    //number of words in input
+//
+//	int state = OUT;
+//
+//	nw = 0;
+//
+//	while ((c=getchar()) != EOF) {
+//		if (c == ' ' || c == '\t' || c == '\n') {
+//			state = OUT;
+//		} else if (state == OUT) {
+//			state = IN;
+//			++nw;
+//		}
+//	}
+//
+//	printf("%d\n", nw);
+//
+//	return 0;
+//}
